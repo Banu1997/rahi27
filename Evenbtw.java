08b0b8e85b7507a74369b9aa385362a843605523
Banu1997 <43529680+Banu1997@users.noreply.github.com>
2018-09-24 20:18:36 +0530

    Evenbtw.java

---
+++ Evenbtw.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Evenbtw
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int N,Q;
+		int i=0;
+		Scanner s=new Scanner(System.in);
+		N=s.nextInt();
+		Q=s.nextInt();
+		
+		
+		
+		for(i=N+1; i<Q; i++)
+        {
+            if(i%2==0)
+                System.out.print(i+" ");
+        }   
+		
+	}
+}
