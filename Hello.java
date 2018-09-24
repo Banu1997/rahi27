ca02e0c1c609aa7578d138c52ecaa96c614b903d
Banu1997 <43529680+Banu1997@users.noreply.github.com>
2018-09-24 13:54:47 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println("Hello");
+		}
+		
+	}
+}
