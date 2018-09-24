1925b03d79b8a5377876d4cfce6d5e2deb9fc9e3
Banu1997 <43529680+Banu1997@users.noreply.github.com>
2018-09-24 14:13:33 +0530

    NumberofDigits.java

---
+++ NumberofDigits.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class NumberofDigits {
+
+    public static void main(String[] args) {
+
+        int count = 0, num;
+        Scanner s=new Scanner(System.in);
+        
+        num=s.nextInt();
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("" + count);
+    }
+}
