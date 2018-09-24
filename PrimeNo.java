d509c753205f23f73fb608f7d665d4ea4aca12fa
Banu1997 <43529680+Banu1997@users.noreply.github.com>
2018-09-24 15:19:29 +0530

    PrimeNo.java

---
+++ PrimeNo.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PrimeNo 
+{
+
+    public static void main(String[] args) {
+
+        int num;
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println( " yes");
+        else
+            System.out.println( "no");
+    }
+}
