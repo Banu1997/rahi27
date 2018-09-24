4d88334df4d286cc98b61f602e1068126d639689
Banu1997 <43529680+Banu1997@users.noreply.github.com>
2018-09-24 11:51:15 +0530

    OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;    
+                
+ class OddEven{
+ 
+        public static void main(String[] args) {
+               
+                
+                     int n=0;  
+                     
+                     {
+                     try
+                      {
+        BufferedReader Buffer= new BufferedReader(new InputStreamReader(System.in));
+                         n = Integer.parseInt(Buffer.readLine());
+                         if(n%2 == 0)
+                                System.out.println( "Even ");
+                         else if(n<0)
+                                     System.out.println("Invalid "); 
+                         else
+                                System.out.println(" Odd ");
+                       } 
+                     catch(Exception e)
+                     {
+                                 System.out.println("invalid ");                       
+                      } 
+                     }
+                    
+                }
+               
+        }
+        
